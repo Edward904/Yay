806343b9ed7bef2e399c30267803e6ca630c9745
100128112 <edward05.jang@gmail.com>
2019-10-08 15:32:44 -0500

    Learning on Python :)

---
+++ YayThisWeek.py
@@ -0,0 +1,12 @@
+# strings and tests
+# the lines right here use % as type of substitution with the proper letters
+x = "There are %d types of animals" % 10
+binary = "binary"
+please doNot = "please don't"
+y = "Those who are familiar with %s and those who %s" % (binary, please doNot)
+
+print(x)
+print(y)
+
+# These require and use quotations and sentences
+print("He said: %r.:" % x)
